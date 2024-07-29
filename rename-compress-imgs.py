8c8d174806f0f43f34051f3c85711e0d4cfa01f9
Omar <omarehap17@gmail.com>
2024-07-29 13:43:44 +0300

    prepare for production

--- rename-compress-imgs.py
+++ rename-compress-imgs.py
@@ -5,18 +5,18 @@ def rename_and_compress_images(directory, max_size_kb=50):
     for filename in os.listdir(directory):
         if filename.endswith(('.png', '.jpg', '.jpeg', ".svg")):
             old_filepath = os.path.join(directory, filename)
-            # new_filename = filename.replace(' ', '-')
-            new_filepath = os.path.join(directory, old_filepath)
+            new_filename = filename.replace(' ', '-')
+            new_filepath = os.path.join(directory, new_filename)
 
             # Rename the file
             os.rename(old_filepath, new_filepath)
             print(f'Renamed: {old_filepath} -> {new_filepath}')
 
-            # Check file size
-            file_size_kb = os.path.getsize(new_filepath) / 1024  # Convert bytes to kilobytes
-            if file_size_kb > max_size_kb:
-                compress_image(new_filepath)
-                print(f'Compressed: {new_filepath} (size: {file_size_kb:.2f} KB)')
+            # # Check file size
+            # file_size_kb = os.path.getsize(new_filepath) / 1024  # Convert bytes to kilobytes
+            # if file_size_kb > max_size_kb:
+            #     compress_image(new_filepath)
+            #     print(f'Compressed: {new_filepath} (size: {file_size_kb:.2f} KB)')
 
 def compress_image(filepath, quality=85):
     # Open an image file
@@ -25,7 +25,8 @@ def compress_image(filepath, quality=85):
         img.save(filepath, optimize=True, quality=quality)
 
 # Specify the directory containing the images
-image_directory = '/mnt/data/projects/django/expert-home/static/imgs/specialization_icons'
+image_directory = '/home/omar/Documents/allahisham-freelance/web-app/UHX/static/imgs/specialization_icons'
+
 
 # Call the function to rename and compress images
 rename_and_compress_images(image_directory)
