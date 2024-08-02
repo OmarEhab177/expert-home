a992f038bb4cf230e661b680bdb327485b973414
Omar <omarehap17@gmail.com>
2024-08-02 10:11:03 +0300

    fix loading and ceo text style

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,52 @@
 $(function () {
-    $(window).on("load", function () {
-        $("#loader").fadeOut(),
-            $("body").removeClass("no-scroll"),
+    $(window).on('load', function () {
+        $('#loader').fadeOut('slow', function () {
+            $('body').removeClass('no-scroll');
             setTimeout(() => {
-                $("#loader").addClass("hidden"), $("#loader").hide();
-            }, 2e3);
-    }),
-        $(window).on("scroll", function (o) {
-            80 < $(this).scrollTop()
-                ? $(".navbar").addClass("navbar-shrink")
-                : $(".navbar").removeClass("navbar-shrink");
-        }),
-        $("#scrollableDiv").on("mouseover", function () {
-            $("#marquee").removeClass("running").addClass("pause");
-        }),
-        $("#scrollableDiv").on("mouseleave", function () {
-            $("#marquee").removeClass("pause").addClass("running");
-        }),
-        $("#navbarSupportedContent a:not(.dropdown-toggle)").on(
-            "click",
-            function () {
-                $("#navbarSupportedContent").collapse("hide");
-            }
-        ),
-        $(".owl-carousel").owlCarousel({
-            loop: !0,
-            rtl: !0,
-            margin: 30,
-            autoplay: !0,
-            autoplayTimeout: 3e3,
-            nav: !0,
-            navText: [
-                '<img src="/static/imgs/icons/arrow-left.png" class="slider-my-arrow-left"></img>',
-                '<img src="/static/imgs/icons/arrow-right.png" class="slider-my-arrow-right"></img>',
-            ],
-            dots: !1,
-            center: !0,
-            responsive: {
-                0: { items: 1 },
-                600: { items: 2 },
-                1e3: { items: 4 },
-            },
-        }),
-        AOS.init();
+                $('#loader').addClass('hidden');
+            }, 1300);
+        });
+    });
+
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 80) {
+            $(".navbar").addClass("navbar-shrink");
+        } else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    $("#scrollableDiv").on("mouseover", function () {
+        $("#marquee").removeClass("running").addClass("pause");
+    });
+
+    $("#scrollableDiv").on("mouseleave", function () {
+        $("#marquee").removeClass("pause").addClass("running");
+    });
+
+    $("#navbarSupportedContent a:not(.dropdown-toggle)").on("click", function () {
+        $("#navbarSupportedContent").collapse("hide");
+    });
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        rtl: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        nav: true,
+        navText: [
+            '<img src="/static/imgs/icons/arrow-left.png" class="slider-my-arrow-left">',
+            '<img src="/static/imgs/icons/arrow-right.png" class="slider-my-arrow-right">'
+        ],
+        dots: false,
+        center: true,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 4 }
+        }
+    });
+
+    AOS.init();
 });
