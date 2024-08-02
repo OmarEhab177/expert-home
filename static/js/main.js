8f84e7c7216bd940215ee9475dab892dc78cd655
Omar <omarehap17@gmail.com>
2024-08-02 10:49:35 +0300

    last try fix fucken loader

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,10 @@
-$(function () {
-    $(document).ready(function () {
-        $(window).on('load', function () {
-            $('#loader').fadeOut('slow', function () {
-                $('body').removeClass('no-scroll');
-                setTimeout(() => {
-                    $('#loader').addClass('hidden');
-                }, 1300);
-            });
+$(document).ready(function () {
+    $(window).on('load', function () {
+        $('#loader').fadeOut('slow', function () {
+            $('body').removeClass('no-scroll');
+            setTimeout(() => {
+                $('#loader').addClass('hidden');
+            }, 1300);
         });
     });
 
